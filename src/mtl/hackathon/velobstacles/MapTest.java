b75e41d3664a2b44d7ad135e234f8269ee0d7e0b
ddaniels4869 <ddaniels4869@gmail.com>
2013-04-27 14:34:54 -0400

    -added GPS (unsure if working, complaint of "Leaked Service")

---
+++ src/mtl/hackathon/velobstacles/MapTest.java
@@ -0,0 +1,17 @@
+package mtl.hackathon.velobstacles;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+public class MapTest extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+    }
+	
+}
